298d5fe33f6482c0a12eae2e0ecbfe872034d257
Willian <w.schmitt2017@gmail.com>
2023-02-24 11:13:46 -0300

    remove video

--- src/pages/ArriveVideo/index.jsx
+++ src/pages/ArriveVideo/index.jsx
@@ -13,15 +13,7 @@ export function ArriveVideo() {
       </div>
         <h1>COMO CHEGAR</h1>
       </div>
-      <iframe 
-        width="760" 
-        height="515" 
-        src="https://www.youtube.com/embed/FBFLzwoKDJ8" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        allowfullscreen>
-      </iframe>
+
           
     </Container>
   )
